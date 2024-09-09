7b0a5c121dbaf1d0a4b2b791afaac0ef7706ad28
Harit007x <harit.ptl.business@gmail.com>
2024-09-09 10:54:00 +0530

    made requried changes to correct build fails and deploymeny

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+const path = require('path');
+const webpack = require("webpack");
 module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    config.resolve.alias = {
+      ...config.resolve.alias,  // preserve existing aliases
+      '@components': path.join(__dirname, 'components'),
+      '@styles': path.join(__dirname, 'styles'),
+      // Add more aliases as needed
+    };
+
+    config.plugins.push(
+      new webpack.ContextReplacementPlugin(/keyv/, (data) => {
+        delete data.dependencies[0].critical;
+        return data;
+      })
+    );
+
+    return config
+  },
   reactStrictMode: true,
-  output: "standalone",
   transpilePackages: ["@repo/ui",],
 };
