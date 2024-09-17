2fda0d5a8df2d3cb94cdff26e86c26522bec89e3
Harit007x <harit.ptl.business@gmail.com>
2024-09-17 12:10:42 +0530

    added import to load env configs

--- apps/test/src/app.ts
+++ apps/test/src/app.ts
@@ -1,9 +1,11 @@
 import http from "http";
 import SocketService from "./services/socket";
+import 'dotenv/config';
 
 const socketService = new SocketService();
 
 const httpServer = http.createServer();
+console.log('port =', process.env.PORT);
 const PORT = process.env.PORT ? process.env.PORT : 8000;
 
 socketService.io.attach(httpServer);
